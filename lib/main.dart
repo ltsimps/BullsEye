baf043a236d116a5e8ed6c103bba8eac319f83ed
ltsimps <ltsimpso@me.com>
2020-12-03 17:03:04 -0500

    silly practice knock knock joke

--- lib/main.dart
+++ lib/main.dart
@@ -30,6 +30,8 @@ class GamePage extends StatefulWidget {
 }
 
 class _GamePageState extends State<GamePage> {
+  bool _alertIsVisible = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -44,15 +46,69 @@ class _GamePageState extends State<GamePage> {
                     TextStyle(fontWeight: FontWeight.bold, color: Colors.green),
               ),
               FlatButton(
-                onPressed: () {},
+                onPressed: () {
+                  this._alertIsVisible = true;
+                  _showAlert(context);
+                  print(" You win ?");
+                },
                 child: Text(
                   'Hit Me!',
                   style: TextStyle(color: Colors.blue),
                 ),
+              ),
+              FlatButton(
+                onPressed: () {
+                  _showKnockKnock(context);
+                },
+                child: Text("Knock Knock"),
               )
             ]),
       ),
       // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+
+  void _showAlert(BuildContext context) {
+    Widget okButton = FlatButton(
+        child: Text('Awesome'),
+        onPressed: () {
+          Navigator.of(context).pop();
+          this._alertIsVisible = false;
+          print("Awesome Pressed!  $_alertIsVisible");
+        });
+
+    showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text("Hello There!"),
+            content: Text("This is my first pop-up."),
+            actions: <Widget>[
+              okButton,
+            ],
+            elevation: 5,
+          );
+        });
+  }
+
+  void _showKnockKnock(BuildContext context) {
+    Widget joke = FlatButton(
+      child: Text("Bad Joke I'm out "),
+      onPressed: () {
+        Navigator.of(context).pop();
+      },
+    );
+
+    showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text("Whos There?"),
+            content: Text('You Dummy'),
+            actions: [
+              joke,
+            ],
+          );
+        });
   }
 }
