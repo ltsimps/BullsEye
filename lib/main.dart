1c8cb0b27a67d1419abe01d3fc63ebdf512867fb
ltsimps <ltsimpso@me.com>
2020-12-10 17:33:52 -0500

    updated score

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:bullseye/game_model.dart';
 import 'package:bullseye/prompt.dart';
 import 'package:bullseye/score.dart';
@@ -63,10 +65,6 @@ class _GamePageState extends State<GamePage> {
                 onPressed: () {
                   this._alertIsVisible = true;
                   _showAlert(context);
-                  setState(() {
-                    _model.totalScore += _pointsForCurrentRound();
-                  });
-                  print(" You win ?");
                 },
                 child: Text(
                   'Hit Me!',
@@ -81,6 +79,11 @@ class _GamePageState extends State<GamePage> {
       ),
       // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+
+  void _nextRound() {
+    _model.target = Random().nextInt(100) + 1;
+    _model.round += 1;
   }
 
   int _sliderValue() => _model.current;
@@ -98,14 +101,17 @@ class _GamePageState extends State<GamePage> {
         onPressed: () {
           Navigator.of(context).pop();
           this._alertIsVisible = false;
-          print("Awesome Pressed!  $_alertIsVisible");
+          setState(() {
+            _model.totalScore += _pointsForCurrentRound();
+            _nextRound();
+          });
         });
 
     showDialog(
         context: context,
         builder: (BuildContext context) {
           return AlertDialog(
-            title: Text("Hello There!"),
+            title: Text(" ${_alertTitle()}"),
             content: Text(
                 "The slider value is ${_sliderValue()}  \nYou scored  ${_pointsForCurrentRound()} points this round"),
             actions: <Widget>[
@@ -114,5 +120,22 @@ class _GamePageState extends State<GamePage> {
             elevation: 5,
           );
         });
+  }
+
+  String _alertTitle() {
+    var difference = (_model.target - _sliderValue()).abs();
+    String title;
+    if (difference == 0)
+      title = "BINGO";
+    else if (difference >= 1 && difference <= 10)
+      title = "So Close!";
+    else if (difference >= 10 && difference <= 20)
+      title = "Almost There!";
+    else if (difference >= 20 && difference <= 50)
+      title = "Getting Warmer!";
+    else
+      title = "Not Even Close!";
+
+    return title;
   }
 }
